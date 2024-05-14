3f9eaa7442cb5963be383c04358e1ab1c12f2343
S-Gull <guillermoalejandro0906@gmail.com>
2024-05-13 20:44:29 -0400

    setso

--- clase1_POO_GA/src/class/libro.php
+++ clase1_POO_GA/src/class/libro.php
@@ -7,7 +7,7 @@ class Libro {
     public $anio_public;
     public $n_hojas;
     
-    
+    //Metodos
     public function __construct()
     {
         $this-> titulo = "titulo por defecto";
@@ -19,33 +19,25 @@ class Libro {
     
 
     }
-    public function imprimir_hoja()
+    public function Imprimir_Hoja()
     {
-        echo "informacion del libro: \n";
-        echo "hoja: {$this->n_hojas}";
+        echo "informacion del nro de hojas: {$this->n_hojas}";
+        
     }
 }
 $libro1 = new Libro();
-$libroNovela = new libro ('Título de la Novela', 'Autor de la Novela', 'Editorial XYZ', 'Segunda Edición', 2022, 400, 'novela');
-$libro2 = new Libro ('Titulo por defecto', "Autor por defecto", "editorial por defecto", "edición por defecto", 2023, 100);
 var_dump($libro1);
-class LibroPrivado 
-    {
+
+class Libro_Protegido 
+    {//* Atributos
         protected $titulo = "titulo por defecto";
-        protected $autor = "autor por defecto";
         protected $editorial = "editorial por defecto";
+        protected $autor = "autor por defecto";
         protected $edicion = "edicion por defecto";
         protected $anio_public = 2023;
         protected $n_hojas = 100;
-
-        public function __construct(
-            $titulo ,
-            $autor ,
-            $editorial ,
-            $edicion ,
-            $anio_public ,
-            $n_hojas 
-        ) {
+//metodos
+        public function __construct($titulo ,$autor ,$editorial ,$edicion ,$anio_public ,$n_hojas) {
             $this->titulo = $titulo;
             $this->autor = $autor;
             $this->editorial = $editorial;
@@ -53,13 +45,16 @@ class LibroPrivado
             $this->anio_public = $anio_public;
             $this->n_hojas = $n_hojas;
         }
-        public function imprimir_informacion()
+        public function Imprimir_Informacion()
         {
             echo "informacion del libro: \n";
             echo "titulo: {$this->titulo}, Autor: {$this -> autor}\n";
         }
 
     }
-
+    $libro_Protec = new Libro_Protegido ('Título de la Novela', 'Autor de la Novela', 'Editorial XYZ', 'Segunda Edición', 2022, 400, 'novela');
+    $libro2 = new Libro ('Titulo por defecto', "Autor por defecto", "editorial por defecto", "edición por defecto", 2023, 100);
+    $libro_Protec ->Imprimir_Informacion();
+    $libro2->Imprimir_Hoja();
 
 ?>
